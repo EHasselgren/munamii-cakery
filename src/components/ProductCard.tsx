42e685dee307d0137324009c9e122259f5830079
elias hasselgren <elias.hasselgren@icloud.com>
2024-10-22 15:05:23 +0200

    Fixed cards to be fixed heigh and width

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,3 +1,5 @@
+
+
 import React from 'react';
 
 interface ProductCardProps {
@@ -8,10 +10,12 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ title, price, image }) => {
   return (
-    <div className="border border-pink-300 bg-white rounded-[140px] shadow-lg p-4 transition-transform transform hover:scale-105 flex flex-col items-center mx-3 overflow-visible">
+    <div className="border border-pink-300 bg-white rounded-[140px] shadow-lg p-4 w-[200px] and h-[300px] transition-transform transform hover:scale-105 flex flex-col items-center mx-3 overflow-visible">
       <img src={image} alt={title} className="w-full h-auto object-cover rounded-full mb-4" />
-      <h4 className="text-2xl font-bold text-[#008080] font-whimsical mb-2">{title}</h4>
-      <p className="text-md text-[#008080] font-whimsical">{price}</p>
+      <div className="text-center">
+        <h4 className="text-2xl font-bold text-[#008080] font-whimsical mb-2">{title}</h4>
+        <p className="text-md text-[#008080] font-whimsical">{price}</p>
+      </div>
     </div>
   );
 };
