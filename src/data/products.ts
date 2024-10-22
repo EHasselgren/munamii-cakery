057083480da57db758c2ee2288626d567dcfc6b2
elias hasselgren <elias.hasselgren@icloud.com>
2024-10-22 18:02:18 +0200

    Added more example products

--- src/data/products.ts
+++ src/data/products.ts
@@ -3,11 +3,24 @@ export const cupcakes = [
   { id: 2, title: 'Vanilla Cupcake', price: '$3', image: '/images/vanillaCupcake.jpg' },
   { id: 3, title: 'Red Velvet Cupcake', price: '$3.5', image: '/images/redVelvetCupcake.jpg' },
   { id: 4, title: 'Lemon Cupcake', price: '$3', image: '/images/lemonCupcake.jpg' },
+  { id: 5, title: 'Strawberry Cupcake', price: '$3.5', image: '/images/vanillaCupcake.jpg' }, 
+  { id: 6, title: 'Carrot Cupcake', price: '$3.5', image: '/images/lemonCupcake.jpg' }, 
+  { id: 7, title: 'Red Velvet with Cream Cheese', price: '$4', image: '/images/redVelvetCupcake.jpg' },
+  { id: 8, title: 'Chocolate Peanut Butter Cupcake', price: '$3.5', image: '/images/chocolateCupcake.jpg' },
+  { id: 9, title: 'Cookies and Cream Cupcake', price: '$3', image: '/images/vanillaCupcake.jpg' }, 
+  { id: 10, title: 'Banana Nut Cupcake', price: '$3.5', image: '/images/lemonCupcake.jpg' }, 
 ];
+
 
 export const weddingCakes = [
   { id: 1, title: 'Classic Wedding Cake', price: '$200', image: '/images/classicWeddingCake.jpg' },
   { id: 2, title: 'Modern Wedding Cake', price: '$250', image: '/images/modernWeddingCake.jpg' },
   { id: 3, title: 'Rustic Wedding Cake', price: '$300', image: '/images/rusticWeddingCake.jpg' },
   { id: 4, title: 'Floral Wedding Cake', price: '$350', image: '/images/floralWeddingCake.jpg' },
+  { id: 5, title: 'Elegant Lace Wedding Cake', price: '$400', image: '/images/classicWeddingCake.jpg' }, 
+  { id: 6, title: 'Naked Wedding Cake', price: '$450', image: '/images/rusticWeddingCake.jpg' }, 
+  { id: 7, title: 'Geode Wedding Cake', price: '$500', image: '/images/modernWeddingCake.jpg' }, 
+  { id: 8, title: 'Vintage Floral Wedding Cake', price: '$550', image: '/images/floralWeddingCake.jpg' }, 
+  { id: 9, title: 'Simple Tiered Wedding Cake', price: '$300', image: '/images/classicWeddingCake.jpg' }, 
+  { id: 10, title: 'Cupcake Tower Wedding Cake', price: '$600', image: '/images/modernWeddingCake.jpg' }, 
 ];
